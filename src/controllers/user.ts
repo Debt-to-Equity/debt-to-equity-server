d4179dde38c6b518a7aa9bf8e24da494b5fb5c01
Cooper Holt <cooperholt@Coopers-MacBook-Pro.local>
2022-03-16 19:41:15 -0600

    can get users

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,9 @@
-import argon2 from 'argon2';
-import validator from 'email-validator';
-import { Request, Response } from 'express';
-import { getRandomString } from '../functions/getRandomString'
+import argon2 from "argon2";
+import validator from "email-validator";
+import { Request, Response } from "express";
+import { getRandomString } from "../functions/getRandomString";
 // import nodemailer from 'nodemailer'
-import { Users as UserModel } from '../models/User'
-
+import { Users as UserModel } from "../models/User";
 
 // let transporter = nodemailer.createTransport({
 //     host: "smtp-mail.outlook.com",
@@ -18,136 +17,161 @@ import { Users as UserModel } from '../models/User'
 // });
 
 export default {
-    createUser: async (req: Request, res: Response) => {
-        let { firstName, lastName, email, phoneNumber } = req.body;
-        const date = new Date()
-        //@ts-ignore
-        let { user } = req.session;
-        const validEmail = await validator.validate(email);
+  createUser: async (req: Request, res: Response) => {
+    let { firstName, lastName, email, phoneNumber, typeOfUser } = req.body;
+    let { userId } = req.params;
+    const date = new Date();
+    //@ts-ignore
+    let { user } = req.session;
+    const validEmail = await validator.validate(email);
 
-        if (!validEmail) {
-            return res.send("Email is Invalid");
-        } else {
-            email = email.toLowerCase();
-            const existingUser = await UserModel.findOne({ email });
-            if (existingUser) {
-                return res.send({ error: 401, message: "Email already in use" });
-            }
+    if (!validEmail) {
+      return res.send("Email is Invalid");
+    } else {
+      email = email.toLowerCase();
+      const existingUser = await UserModel.findOne({ email });
+      if (existingUser) {
+        return res.send({ error: 401, message: "Email already in use" });
+      }
 
-            let token = getRandomString(6);
+      let token = getRandomString(6);
 
-            let createdUser = new UserModel({
-                firstName,
-                lastName,
-                email,
-                phoneNumber,
-                password: null,
-                parentId: '620dab677c0082f4b08cb471',
-                typeOfUser: 'client',
-                address: null,
-                createdAt: date,
-                updatedAt: date,
-                token: {
-                    string: token,
-                    createdAt: date
-                }
-            });
+      let createdUser = new UserModel({
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        password: null,
+        parentId: userId,
+        typeOfUser,
+        address: null,
+        createdAt: date,
+        updatedAt: date,
+        token: {
+          string: token,
+          createdAt: date,
+        },
+      });
 
-            const user = await createdUser.save()
+      const user = await createdUser.save();
 
-            // transporter.sendMail({
-            //     from: "coopergoldenholt@outlook.com", // sender address
-            //     to: email, // list of receivers
-            //     subject:
-            //         "You Have Been Invited To Register For the Debt to Equity App!", // Subject line
-            //     html: `<p><b>Hello</b>  </p>
-            // <p>You have been invited to register an account with Debt to Equity! Download the app in your app store and register a user with this token! ${token}
-            // This token will only last 24 hours.
-            // </p>`,
-            // });
+      // transporter.sendMail({
+      //     from: "coopergoldenholt@outlook.com", // sender address
+      //     to: email, // list of receivers
+      //     subject:
+      //         "You Have Been Invited To Register For the Debt to Equity App!", // Subject line
+      //     html: `<p><b>Hello</b>  </p>
+      // <p>You have been invited to register an account with Debt to Equity! Download the app in your app store and register a user with this token! ${token}
+      // This token will only last 24 hours.
+      // </p>`,
+      // });
 
-            return res.send({
-                email: user.email,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                loggedIn: true,
-                userType: user.typeOfUser,
-                id: user._id,
-                phoneNumber: user.phoneNumber,
-                parentId: user.parentId,
-                address: user.address
-            });
-        }
-    },
-    loginUser: async (req, res) => {
-        let { email, password } = req.body;
-
-        email = email.toLowerCase();
-        const user = await UserModel.findOne({ email });
-        if (!user) {
-            return res.send({ error: 401, message: "Email or Password incorrect." });
-        }
-        const valid = await argon2.verify(user.password, password);
-
-        if (!valid) {
-            return res.send({ error: 401, message: "Email or Password incorrect." });
-
-        }
-
-        req.session!.user = {
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            loggedIn: true,
-            userType: user.typeOfUser,
-            id: user._id,
-            phoneNumber: user.phoneNumber,
-            parentId: user.parentId,
-            address: user.address
-        };
-
-        return res.send(req.session!.user)
-
-    },
-    changePassword: async (req, res) => {
-        let { email, password, token, phoneNumber } = req.body;
-        email = email.toLowerCase();
-        let user = await UserModel.findOne({ email });
-        if (!user) {
-            return res.send({ error: 401, message: "Email could not be found." });
-        };
-        // console.log(user.token.createdAt.addDays('3'));
-
-        if (user.token.string !== token) {
-            return res.send({ error: 401, message: "Incorrect Token" });
-        };
-
-        // if (user.token.createdAt > user.token.createdAt.addDays('3')) {
-        //     return res.sed({ error: 400, message: "Token is expired" })
-        // };
-
-        if (user.phoneNumber !== phoneNumber) {
-            return res.send({ error: 401, message: "Phone Numbers do not match." });
-        };
-
-        const hashedPassword = await argon2.hash(password);
-
-        user.password = hashedPassword;
-
-        user.save();
-
-        req.session!.user = {
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            loggedIn: true,
-            userType: user.typeOfUser,
-            id: user._id,
-            phoneNumber: user.phoneNumber,
-            parentId: user.parentId,
-            address: user.address
-        };
-
-        res.send(req.session.user)
+      return res.send({
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        loggedIn: true,
+        userType: user.typeOfUser,
+        id: user._id,
+        phoneNumber: user.phoneNumber,
+        parentId: user.parentId,
+        address: user.address,
+      });
     }
-}
+  },
+  loginUser: async (req, res) => {
+    let { email, password } = req.body;
+
+    email = email.toLowerCase();
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.send({ error: 401, message: "Email or Password incorrect." });
+    }
+    const valid = await argon2.verify(user.password, password);
+
+    if (!valid) {
+      return res.send({ error: 401, message: "Email or Password incorrect." });
+    }
+
+    req.session!.user = {
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      loggedIn: true,
+      userType: user.typeOfUser,
+      id: user._id,
+      phoneNumber: user.phoneNumber,
+      parentId: user.parentId,
+      address: user.address,
+    };
+
+    return res.send(req.session!.user);
+  },
+  changePassword: async (req, res) => {
+    let { email, password, token, phoneNumber } = req.body;
+    email = email.toLowerCase();
+    token = token.toLowerCase();
+    let user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.send({ error: 401, message: "Email could not be found." });
+    }
+
+    if (user.token.string !== token) {
+      return res.send({ error: 401, message: "Incorrect Token" });
+    }
+
+    // if (user.token.createdAt > user.token.createdAt.addDays('3')) {
+    //     return res.sed({ error: 400, message: "Token is expired" })
+    // };
+
+    if (user.phoneNumber !== phoneNumber) {
+      return res.send({ error: 401, message: "Phone Numbers do not match." });
+    }
+
+    const hashedPassword = await argon2.hash(password);
+
+    user.password = hashedPassword;
+
+    try {
+      user.save();
+    } catch {
+      res.send({
+        error: 500,
+        message: "User could not be saved. Please try again later.",
+      });
+    }
+
+    req.session!.user = {
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      loggedIn: true,
+      userType: user.typeOfUser,
+      id: user._id,
+      phoneNumber: user.phoneNumber,
+      parentId: user.parentId,
+      address: user.address,
+    };
+
+    res.send(req.session.user);
+  },
+  getChildUsers: async (req, res) => {
+    const { userId } = req.params;
+    const childUsers = await UserModel.find({ parentId: userId });
+
+    const cleanedUsers = childUsers.map((user) => {
+      return {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userType: user.typeOfUser,
+        id: user._id,
+        phoneNumber: user.phoneNumber,
+        parentId: user.parentId,
+        address: user.address,
+      };
+    });
+
+    res.send(cleanedUsers);
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
 import "reflect-metadata";
 require("dotenv").config();
 import express from "express";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import session from "express-session";
 const { PORT, MONGO_DB_URI } = process.env;
-import userCtrl from './controllers/user';
-import expectedCtrl from './controllers/budget'
-import debtCtrl from './controllers/debt'
+import userCtrl from "./controllers/user";
+import expectedCtrl from "./controllers/budget";
+import debtCtrl from "./controllers/debt";
 import revenue from "./controllers/revenue";
 
 const main = async () => {
-
   const app = express();
 
   app.use(express.json());
 
   app.use(
     session({
-      secret: 'jhkjhkljhlkhiuihkn kjhiuh87yt7ezsxd',
+      secret: "jhkjhkljhlkhiuihkn kjhiuh87yt7ezsxd",
       resave: false,
       saveUninitialized: true,
       cookie: {
@@ -32,16 +31,17 @@ const main = async () => {
   //@ts-ignore
   const server = mongoose.connect(MONGO_DB_URI);
 
-  app.post('/login', userCtrl.loginUser);
-  app.post('/register', userCtrl.createUser);
-  app.post('/password', userCtrl.changePassword)
+  app.post("/login", userCtrl.loginUser);
+  app.post("/register/:userId", userCtrl.createUser);
+  app.post("/password", userCtrl.changePassword);
 
-  app.get('/payoff/:userId', debtCtrl.getPayoff);
-  app.get('/debt/:userId', debtCtrl.getDebts);
+  app.get("/payoff/:userId", debtCtrl.getPayoff);
+  app.get("/debt/:userId", debtCtrl.getDebts);
+  app.get("/user/children/:userId", userCtrl.getChildUsers);
 
-  app.post('/revenue/:userId', revenue.insertMultipleRevenue);
-  app.post('/debt/:userId', debtCtrl.insertMultipleDebts);
-  app.post('/budget/:userId', expectedCtrl.insertBudget);
+  app.post("/revenue/:userId", revenue.insertMultipleRevenue);
+  app.post("/debt/:userId", debtCtrl.insertMultipleDebts);
+  app.post("/budget/:userId", expectedCtrl.insertBudget);
 
   app.listen(PORT, () => {
     console.log("server started on localhost:4000");
